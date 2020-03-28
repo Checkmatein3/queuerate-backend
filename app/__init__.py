448ed872020cc7e7bee716e91186d67189e06a31
Daniel <danielkolodzi@gmail.com>
2020-03-27 23:43:21 -0400

    change flask app to run for production

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 import os
 
-app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origin": "http://localhost:8081"}})
-app.config.from_object('config')
+application = Flask(__name__)
+cors = CORS(application, resources={r"/*": {"origin": "http://localhost:8081"}})
+application.config.from_object('config')
 
 def get_env_variable(name):
     try:
@@ -23,22 +23,22 @@ POSTGRES_DB = get_env_variable("POSTGRES_DB")
 
 DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
+application.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
 
 # Database configuration
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
 # import models so the migration util will pick them up
-from app import models
+from application import models
 
 # Module imports - for registering blueprints
-from app.category.controllers import category_controller as category_module
-from app.keyword.controllers import keyword_controller as keyword_module
-from app.user.controllers import user_controller as user_module
-from app.forwarding_settings.controllers import fs_controller as fs_module
-from app.link.controllers import link_controller as link_module
-from app.RelevantKeywords.controllers import rk_controller as rk_module
+from application.category.controllers import category_controller as category_module
+from application.keyword.controllers import keyword_controller as keyword_module
+from application.user.controllers import user_controller as user_module
+from application.forwarding_settings.controllers import fs_controller as fs_module
+from application.link.controllers import link_controller as link_module
+from application.RelevantKeywords.controllers import rk_controller as rk_module
 
 main = Blueprint("main", __name__)
 
@@ -52,10 +52,10 @@ def index():
     return "Hello World"
 
 
-app.register_blueprint(main, url_prefix='/')
-app.register_blueprint(category_module, url_prefix='/categories')
-app.register_blueprint(keyword_module, url_prefix='/keywords')
-app.register_blueprint(user_module, url_prefix='/users')
-app.register_blueprint(fs_module, url_prefix='/forwarding_settings')
-app.register_blueprint(link_module, url_prefix='/links')
-app.register_blueprint(rk_module, url_prefix='/relevant_keywords')
+application.register_blueprint(main, url_prefix='/')
+application.register_blueprint(category_module, url_prefix='/categories')
+application.register_blueprint(keyword_module, url_prefix='/keywords')
+application.register_blueprint(user_module, url_prefix='/users')
+application.register_blueprint(fs_module, url_prefix='/forwarding_settings')
+application.register_blueprint(link_module, url_prefix='/links')
+application.register_blueprint(rk_module, url_prefix='/relevant_keywords')
